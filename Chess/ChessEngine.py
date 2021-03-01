afa9dbaa9f8d2e49bc05e98f609752486bf24be7
rhowlader5750 <rhowlade@stevens.edu>
2021-02-28 20:02:08 -0500

    file updated

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -22,3 +22,101 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
 
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)  # logs the moves so we can undo later
+        self.whiteToMove = not self.whiteToMove  # swap players
+
+    '''
+    Undo the last move made 
+    '''
+
+    def undoMove(self):
+        if len(self.moveLog) != 0: #makes sure that there is a move to undo
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove # switch turn back
+
+    '''
+    This will generate what moves aer correct besides check  
+    '''
+
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    '''
+    This will generafe all the possible moves in the board  
+    '''
+    def getAllPossibleMoves(self):
+        moves = [Move((6, 4), (4, 4), self.board)]
+        for r in range(len(self.board)):  #number of rows
+            for c in range(len(self.board[r])):   # number of columns in a given row
+               turn = self.board[r][c][0]
+               if(turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                  piece = self.board[r][c][1]
+                  if piece == 'p':
+                      self.getPawnMoves(r, c, moves)
+                  elif piece == 'R':
+                      self.getRookMoves(r, c, moves)
+        return moves
+
+
+    '''
+    Gets the moves of the pawn 
+    '''
+    def getPawnmoves(self, r, c, moves):
+        pass
+
+    '''
+    Gets the moves of the rook
+    '''
+    def getRookmoves(self, r, c, moves):
+        pass
+
+
+class Move():
+    # maps keys to values
+    # key : values
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+
+    '''
+    Overiding the equals to method 
+    '''
+
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+
+
+
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endCol, self.endCol)
+
+
+
+    def getRankFile(self,r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
+
+
+
+
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -29,12 +29,49 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False #flag varible for when a move is made
     LoadImages()
     running = True
+    sqSelected = ()  #no square is selected, keep track of the last click of the user( tuple: (row, column)
+    playerClicks = []  #keep track of the player clicks (two tuples: [(6,4) , (4, 4)])
+
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+               location = p.mouse.get_pos() #(x,y) location of mouse
+               col = location[0] // SQ_SIZE
+               row = location[1] // SQ_SIZE
+               if sqSelected == (row, col): # when the user clicks twice means undo
+                   sqSelected = () #deselcted
+                   playerClicks = [] # clear player click
+               else:
+                   sqSelected =(row, col)
+                   playerClicks.append(sqSelected) #append for both 1st and second click
+               if len(playerClicks) == 2: # after 2nd click
+                   move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                   print(move.getChessNotation())
+                   if move in validMoves:
+                       gs.makeMove(move)
+                       moveMade =True
+                   sqSelected = ()
+                   playerClicks = []
+            # key handlers
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: # UNDO WHEN Z IS PRESSEDE
+                   gs.undoMove()
+                   moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
+
+
+
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
